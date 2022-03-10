e8e988bac512c190151ffa27aaad6a4804c30d14
bukowy <bukowy@msoe.edu>
2022-03-10 16:02:53 -0600

    End class w1d2

---
+++ classExamples/src/week1/DayOneDriver.java
@@ -0,0 +1,27 @@
+package week1;
+
+import java.util.Scanner;
+
+public class DayOneDriver {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter a numerator");
+        int num = Integer.parseInt(in.nextLine());
+
+        System.out.println("Please enter a denominator");
+        int den = Integer.parseInt(in.nextLine());
+
+
+        if(den != 0 && (num / den) > 3){
+            System.out.println("yay! your fraction is greater greater than 3");
+            System.out.println("Your number was: " + num/den);
+        }
+
+
+
+    }
+
+}
